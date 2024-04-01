a5cef1521087e6f7558e401cd21c808ce37bd2f1
Just_Ho_it <zalop3128@kakao.com>
2024-04-01 15:54:52 +0900

    Initial Settings

---
+++ src/main/java/com/example/spirngsecurityinaction/SpirngSecurityInActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.spirngsecurityinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngSecurityInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngSecurityInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spirngsecurityinaction/SpirngSecurityInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spirngsecurityinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngSecurityInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
