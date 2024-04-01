5c8b6b799c2df357f9dd8cabea294891970e71ff
Just_Ho_it <zalop3128@kakao.com>
2024-04-01 16:34:13 +0900

    1-3. Security Config - login form test

--- src/main/java/com/example/spirngsecurityinaction/config/SecurityConfig.java
+++ src/main/java/com/example/spirngsecurityinaction/config/SecurityConfig.java
@@ -13,13 +13,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
-        httpSecurity
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
-                        .anyRequest().authenticated()
-                );
+
         /*
          * permitAll - 모든 사용자에게 로그인 하지 않아도 허용
          * hasRole - 특정한 규칙이 있어야 접근 허용
@@ -27,6 +21,24 @@ public class SecurityConfig {
          * authenticated - 로그인만 진행하면 접근 허용
          * denyAll - 모든 이용자 접근 비허용
          * */
+        httpSecurity
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .anyRequest().authenticated()
+                );
+
+        httpSecurity
+                .csrf((auth) -> auth.disable()
+                );
+
+        httpSecurity
+                .formLogin((auth) -> auth.loginPage("/")
+                        .loginProcessingUrl("/loginProc")
+                        .permitAll()
+                );
+
         return httpSecurity.build();
     }
 }
--- src/main/java/com/example/spirngsecurityinaction/controller/LoginController.java
+++ src/main/java/com/example/spirngsecurityinaction/controller/LoginController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdminController {
+public class LoginController {
 
-    @GetMapping("/admin")
-    public String adminPage() {
+    @GetMapping("/login")
+    public String loginPage() {
 
-        return "admin";
+        return "login";
     }
 }
