e941eb6fd08377268027afcdfc75529cecd37861
Just_Ho_it <zalop3128@kakao.com>
2024-04-03 00:00:47 +0900

    4-2. security - 세션 설정(소멸, 중복 로그인, 고정보호)

--- src/main/java/com/example/spirngsecurityinaction/config/SecurityConfig.java
+++ src/main/java/com/example/spirngsecurityinaction/config/SecurityConfig.java
@@ -20,15 +20,14 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
-        /*
-         * permitAll - 모든 사용자에게 로그인 하지 않아도 허용
-         * hasRole - 특정한 규칙이 있어야 접근 허용
-         * hasAnyRole - 여러가지 규칙 설정
-         * authenticated - 로그인만 진행하면 접근 허용
-         * denyAll - 모든 이용자 접근 비허용
-         * */
         httpSecurity
+                /*
+                 - permitAll - 모든 사용자에게 로그인 하지 않아도 허용
+                 - hasRole - 특정한 규칙이 있어야 접근 허용
+                 - hasAnyRole - 여러가지 규칙 설정
+                 - authenticated - 로그인만 진행하면 접근 허용
+                 - denyAll - 모든 이용자 접근 비허용
+                 * */
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login", "/loginProc", "/signUp", "/signUpProc").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
@@ -45,6 +44,26 @@ public class SecurityConfig {
                         .loginProcessingUrl("/loginProc")
                         .permitAll()
                 );
+
+        httpSecurity
+                /*
+                 - maximumSession() : 숫자 만큼 다중 로그인 허용 개수
+                 - maxSessionPreventsLogin() : true || false 다중 로그인 개수를 초과하였을 경우 처리 방법
+                 * true -> 초과시 새로운 로그인 차단
+                 * false -> 초과시 기존 세션 하나 삭제
+                 * */
+                .sessionManagement((auth) -> auth
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        httpSecurity
+                /*
+                - sessionManagement().sessionFixation().none() : 로그인 시 세션 정보 변경 안함
+                - sessionManagement().sessionFixation().newSession() : 로그인 시 세션 새로 생성
+                - sessionManagement().sessionFixation().changeSessionId() : 로그인 시 동일한 세션에 대한 id 변경
+                * */
+                .sessionManagement((auth) -> auth
+                        .sessionFixation().changeSessionId());
 
         return httpSecurity.build();
     }
